b5337f425dc44385a1b0aa5501e09f1477773fa1
seokmin01 <22000666@handong.edu>
2021-09-11 23:08:04 +0900

    add comparator interface

--- src/com/example/lab2/Fruit.java
+++ src/com/example/lab2/Fruit.java
@@ -1,9 +1,10 @@
 package com.example.lab2;
+import java.util.Comparator;
 
 public class Fruit implements Comparable<Fruit>{
-	private int no;
-	private String name;
-	private int price;
+	int no;
+	String name;
+	int price;
 	
 	public Fruit() {}
 	public Fruit(int no, String name, int price) {
@@ -43,4 +44,20 @@ public class Fruit implements Comparable<Fruit>{
 		return name.compareTo(o.name);
 	}
 	
+}
+
+class FruitComparator implements Comparator <Fruit> {
+
+	@Override
+	public int compare(Fruit o1, Fruit o2) {
+		return o1.name.compareTo(o2.name);
+	}
+}
+
+class FruitComparatorDesc implements Comparator <Fruit> {
+
+	@Override
+	public int compare(Fruit o1, Fruit o2) {
+		return o2.name.compareTo(o1.name);
+	}
 }
